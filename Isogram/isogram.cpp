b88afe033bbbd67c7c06a126f300093130651fb9
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-24 15:26:25 +0200

    first commit

---
+++ Isogram/isogram.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+string toLowerCase(string word){
+    string newWord = "";
+
+    for (int i = 0; i < word.length(); i++){
+        newWord += (char)tolower(word[i]);
+    }
+
+    return newWord;
+}
+
+bool isIsogram(string word){
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    //Convert the word to lowercase before comparing each character
+    word = toLowerCase(word);
+
+    for (int i = 0; i < word.length(); i++){
+        // string::npos is a constant number 4294967295 that represents not found
+        if (alphabet.find(word[i]) != string::npos){
+            alphabet.erase(remove(alphabet.begin(), alphabet.end(), word[i]), alphabet.end());
+        }
+        else
+            return false; 
+    }
+    return true;
+}
+
+void printResult(string word){
+    if (isIsogram(word) == true){
+        cout << word + " is an isogram\n";
+    }
+    else{
+        cout << word + " has repeating letters and is not an isogram\n";
+    }
+}
+
+int main(){
+    string word1 = "Mel";
+    string word2 = "Stilling";
+    
+    //Print the result of isIsogram(word1) to the console
+    printResult(word1);
+
+    //Print the result of isIsogram(word2) to the console
+    printResult(word2);
+        
+    return 0;
+}
