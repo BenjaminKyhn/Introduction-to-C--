fcf6b5b34c4f41bb0fa202f48776cb36e5fbdedb
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-24 16:56:35 +0200

    reference.cpp complete

--- PassByReferenceDemo/reference.cpp
+++ PassByReferenceDemo/reference.cpp
@@ -2,29 +2,49 @@
 #include <string>
 using namespace std;
 
-void passByValue(string word);
-void passByReference(string *ptr);
+void toLowercase(string word);
+void toLowercase(string *ptr);
 
 int main(){
     // A String without a pointer
-    string word1 = "Chernobyl";
+    string word1 = "CHERNOBYL";
 
     // A string that also has a pointer
-    string word2 = "Chernobyl";
+    string word2 = "CHERNOBYL";
     string* ptr = &word2;
 
-    // Apply transformation to the words
-    passByValue(word1);
-    passByReference(ptr);
+    // Call the toLowercase function by passing a copy of word1 as parameter
+    toLowercase(word1);
 
+    // Call the toLowercase function by passing the reference to word2 as parameter
+    toLowercase(ptr);
+
+    // Observe that word1 hasn't changed
     cout << word1 << "\n";
+
+    // Observe that word2 has changed
     cout << word2 << "\n";
 }
 
-void passByValue(string word){
-    word = "Transformed word";
+void toLowercase(string word){
+    string newWord = "";
+
+    for (int i = 0; i < word.length(); i++){
+        newWord += (char)tolower(word[i]);
+    }
+
+    word = newWord;
 }
 
-void passByReference(string *ptr){
-    *ptr = "Transformed word";
+void toLowercase(string *ptr){
+    // *ptr isn't a string, so we can't call string functions on it
+    // Therefore we create a new string variabel from *ptr so we can call string functions like length()
+    string word = *ptr;
+    string newWord = "";
+
+    for (int i = 0; i < word.length(); i++){
+        newWord += (char)tolower(word[i]);
+    }
+
+    *ptr = newWord;
 }
